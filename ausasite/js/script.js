2dece427746f85ec01555d411161bd04b163f88b
JaeMinBird <jaeminbird@gmail.com>
2023-09-20 16:59:11 -0400

    redid navbar

--- ausasite/js/script.js
+++ ausasite/js/script.js
@@ -1,2 +1,15 @@
-document.querySelector('.second-button').addEventListener('click', function () {
-document.querySelector('.animated-icon2').classList.toggle('open'); });
+// Function to toggle the hamburger menu animation
+function toggleMenuAnimation() {
+    const icon = document.querySelector('.animated-icon2'); // Change to the appropriate selector
+  
+    if (icon.classList.contains('open')) {
+      icon.classList.remove('open');
+    } else {
+      icon.classList.add('open');
+    }
+}
+  
+// Add a click event listener to the menu button
+const menuButton = document.querySelector('.navbar-toggler');
+menuButton.addEventListener('click', toggleMenuAnimation);
+  
