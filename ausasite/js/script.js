2c8b4176efc209403294924736cc8a32449fad7e
JaeMinBird <jaeminbird@gmail.com>
2023-10-09 16:13:15 -0400

    added parallax, removed form script

--- ausasite/js/script.js
+++ ausasite/js/script.js
@@ -1,3 +1,18 @@
+//parallax
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 // Function to toggle the hamburger menu animation
 function toggleMenuAnimation() {
     const icon = document.querySelector('.animated-icon2'); // Change to the appropriate selector
@@ -12,4 +27,3 @@ function toggleMenuAnimation() {
 // Add a click event listener to the menu button
 const menuButton = document.querySelector('.navbar-toggler');
 menuButton.addEventListener('click', toggleMenuAnimation);
-  
