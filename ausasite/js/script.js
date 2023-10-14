d423502279527113193fd80f90cf55f6c20ec5f2
JaeMinBird <jaeminbird@gmail.com>
2023-10-14 16:52:05 -0400

    added bamboo

--- ausasite/js/script.js
+++ ausasite/js/script.js
@@ -1,5 +1,21 @@
-//parallax
-const observer = new IntersectionObserver((entries) => {
+const elementsToRemove = document.querySelectorAll('.bamboo');
+
+function removeElementsIfWidthBelowThreshold() {
+  if (window.innerWidth <= 991) {
+    elementsToRemove.forEach((element) => {
+      element.remove();
+    });
+  }
+}
+
+// Call the function initially and add an event listener to handle window resizing
+removeElementsIfWidthBelowThreshold();
+window.addEventListener('resize', removeElementsIfWidthBelowThreshold);
+
+
+
+//parallax for eboard cards
+const observer2 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.querySelector('.hidden').classList.add('show');
@@ -10,12 +26,12 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenTrigger = document.querySelector('.hidetrig');
-observer.observe(hiddenTrigger);
+observer2.observe(hiddenTrigger);
 
 
 // Function to toggle the hamburger menu animation
 function toggleMenuAnimation() {
-    const icon = document.querySelector('.animated-icon2'); // Change to the appropriate selector
+    const icon = document.querySelector('.animated-icon2'); // Change to appropriate selector
   
     if (icon.classList.contains('open')) {
       icon.classList.remove('open');
@@ -24,6 +40,6 @@ function toggleMenuAnimation() {
     }
 }
   
-// Add a click event listener to the menu button
+// Click event listener for menu button
 const menuButton = document.querySelector('.navbar-toggler');
 menuButton.addEventListener('click', toggleMenuAnimation);
