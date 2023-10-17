ad500d4de98396f07973d9673fe5f512579eea5e
JaeMinBird <jaeminbird@gmail.com>
2023-10-16 21:04:02 -0400

    fixed js errors and optimizing css

--- ausasite/js/script.js
+++ ausasite/js/script.js
@@ -1,3 +1,31 @@
+// Function to toggle the hamburger menu animation
+function toggleMenuAnimation() {
+  const icon = document.querySelector('.animated-icon2'); // Change to appropriate selector
+
+  if (icon.classList.contains('open')) {
+    icon.classList.remove('open');
+  } else {
+    icon.classList.add('open');
+  }
+}
+
+// Click event listener for menu button
+const menuButton = document.querySelector('.navbar-toggler');
+menuButton.addEventListener('click', toggleMenuAnimation);
+
+// Extract data from form
+function submitForm(event) {
+event.preventDefault(); // Prevent the form from reloading the page
+
+// Get the form data
+const name = document.getElementById("nameInput").value;
+const email = document.getElementById("emailInput").value;
+
+// Do something with the data
+console.log("Name:", name);
+console.log("Email:", email);
+}
+
 //remove bamboo on smaller windows
 const elementsToRemove = document.querySelectorAll('.bamboo');
 
@@ -26,33 +54,4 @@ const observer2 = new IntersectionObserver((entries) => {
 });
 
 const hiddenTrigger = document.querySelector('.hidetrig');
-observer2.observe(hiddenTrigger);
-
-
-// Function to toggle the hamburger menu animation
-function toggleMenuAnimation() {
-    const icon = document.querySelector('.animated-icon2'); // Change to appropriate selector
-  
-    if (icon.classList.contains('open')) {
-      icon.classList.remove('open');
-    } else {
-      icon.classList.add('open');
-    }
-}
-  
-// Click event listener for menu button
-const menuButton = document.querySelector('.navbar-toggler');
-menuButton.addEventListener('click', toggleMenuAnimation);
-
-// Extract data from form
-function submitForm(event) {
-  event.preventDefault(); // Prevent the form from reloading the page
-
-  // Get the form data
-  const name = document.getElementById("nameInput").value;
-  const email = document.getElementById("emailInput").value;
-
-  // Do something with the data
-  console.log("Name:", name);
-  console.log("Email:", email);
-}
+observer2.observe(hiddenTrigger); // always put this at the bottom because it causes errors
